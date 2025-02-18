440dafe573ee3f603f138534deee8cd361dbb1d5
Rônero <52432170+ronerog@users.noreply.github.com>
2025-02-18 13:28:25 -0300

    Add => configuracao inicial webhook, deploy na vercel

--- api/index.js
+++ api/index.js
@@ -6,14 +6,18 @@ const webhookRoutes = require("../routes/webhook.routes");
 dotenv.config();
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use("/", webhookRoutes);
+
+app.use("", webhookRoutes);
 
 app.options("/webhook", (req, res) => {
     res.set("Allow", "POST, OPTIONS");
     res.sendStatus(200);
 });
 
-// Exporte o servidor para a Vercel reconhecer
-module.exports = app;
+const PORT = process.env.PORT || 3005;
+app.listen(PORT, () => {
+    console.log(`Webhook rodando na porta ${PORT}`);
+});
